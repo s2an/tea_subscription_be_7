88d469fd3e45406c78d5a4211470129b74912e41
Nash <141100397+s2an@users.noreply.github.com>
2024-03-21 12:56:34 -0400

    test: Create happy and sad path tests

--- spec/requests/api/v1/subscriptions_spec.rb
+++ spec/requests/api/v1/subscriptions_spec.rb
@@ -2,7 +2,7 @@
 require "rails_helper"
 
 RSpec.describe "Subscriptions", type: :request do
-  describe "GET /customers/:customer_id/subscriptions/:id" do
+  describe "GET /customers/:customer_id/subscriptions" do
     it "returns all of the customer's subscriptions" do
       customer = Customer.create!(first_name: "Jane", last_name: "Doe", email: "jane.doe@anyominous.com", address: "123 Main St")
       tea = Tea.create!(title: "Green Tea", description: "Green tea lighter than Black Tea.", temperature: 180, brew_time: 5)
@@ -11,10 +11,38 @@ RSpec.describe "Subscriptions", type: :request do
       get api_v1_customer_subscriptions_path(customer)
       
       expect(response).to have_http_status(:success)
+      expect
       expect(response.body).to include(subscription.title)
       expect(response.body).to include(subscription.price.to_s)
       expect(response.body).to include(subscription.status)
       expect(response.body).to include(subscription.frequency)
+    end
+  end
+
+  describe "POST /customers/:customer_id/subscriptions" do
+    it "HAPPY PATH: creates a new subscription for the customer" do
+      customer = Customer.create!(first_name: "Jane", last_name: "Doe", email: "jane.doe@anyominous.com", address: "123 Main St")
+      subscription_params = { subscription: { title: "The Green Box", price: 9.99, status: "active", frequency: "monthly" } }
+
+      post api_v1_customer_subscriptions_path(customer), params: subscription_params
+
+      expect(response).to have_http_status(:created)
+      expect(customer.subscriptions.count).to eq(1)
+
+      expect(response.body).to include("The Green Box")
+      expect(response.body).to include("9.99")
+      expect(response.body).to include("active")
+      expect(response.body).to include("monthly")
+    end
+
+    it "SAD PATH: does not create a new subscription with incomplete params" do
+      customer = Customer.create!(first_name: "Jane", last_name: "Doe", email: "jane.doe@anyominous.com", address: "123 Main St")
+      incomplete_subscription_params = { subscription: { title: "The Green Box", price: 9.99 } }
+
+      post api_v1_customer_subscriptions_path(customer), params: incomplete_subscription_params
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(customer.subscriptions.count).to eq(0)
     end
   end
 end
