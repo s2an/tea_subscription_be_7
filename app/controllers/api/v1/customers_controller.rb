05861b467925be5b9f61677c9159ad6a1bb211f1
Nash <141100397+s2an@users.noreply.github.com>
2024-04-01 14:34:17 -0400

    Add customer create action and verify through Postman

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,6 +1,22 @@
 class Api::V1::CustomersController < ApplicationController
+  
+  def create
+    customer = Customer.new(customer_params)
+    if customer.save
+      render json: customer, status: :created
+    else
+      render json: customer.errors, status: :unprocessable_entity
+    end
+  end
+
   def show
     customer = Customer.find(params[:id])
     render json: customer
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do  
       resources :teas, only: [:show]
-      resources :customers, only: [:show] do
+      resources :customers, only: [:show, :create] do
         resources :subscriptions, only: [:index, :create, :destroy]
       end
     end
